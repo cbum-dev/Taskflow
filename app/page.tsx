ba2d33791d5c75379099523a0d335ed812c316b9
cbum-dev <priyanshukumar2002234@gmail.com>
2025-01-04 14:19:51 +0530

    added shadcnui

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,136 @@ import { useSession, signIn, signOut } from "next-auth/react";
 export default function Component() {
   const { data: session } = useSession();
   if (session) {
+    console.log(session);
+    console.log(session.user);
     return (
-      <>
-        Signed in as {session.user.email} <br />
+      <div>
+        Signed in as {session.user.emai || session.user?.name} <br />
+        {/* Signed in as {session.user} <br /> */}
+        {/* Signed in as {session} <br /> */}
         <button onClick={() => signOut()}>Sign out</button>
-      </>
+      </div>
     );
   }
   return (
-    <>
+    <div className="">
+      <div className="h-96 w-96 bg-slate-500 overflow-auto no-scrollbar text-nowrap">
+
+ <h1 className="text-3xl over">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1> <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1> <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1> <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>
+      </div>
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>{" "}
+      <h1 className="text-3xl">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam odio
+        incidunt impedit quo non vero quasi labore alias similique ipsam!
+        Temporibus iusto, tempore laudantium adipisci molestiae soluta fuga
+        sapiente ratione!
+      </h1>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+      <button onClick={() => signIn("google")}>Sign in with google</button>
+      <button onClick={() => signIn("github")}>Sign in with github</button>
+    </div>
   );
 }
